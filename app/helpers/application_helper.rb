50ffee725990fbf4725473f61fd51e34a8a8268c
Phuc <phuc.t.vo.bkdn@gmail.com>
2015-09-29 12:43:26 +0700

    Add full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+	def full_title(page_title = '')
+    		base_title = "Tim Tro Nhanh"
+    		if page_title.empty?
+      			base_title
+    		else
+      			page_title + " | " + base_title
+    		end
+  	end
 end
