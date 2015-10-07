411192b73f2b794c9589b0e43d349ef0157e1299
Phuc <phuc.t.vo.bkdn@gmail.com>
2015-10-07 08:45:51 +0700

    update

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150930040145) do
+ActiveRecord::Schema.define(version: 20151006153721) do
+
+  create_table "posts", force: :cascade do |t|
+    t.text     "desc",       limit: 65535
+    t.integer  "user_id",    limit: 4
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+    t.string   "location",   limit: 255
+    t.string   "type_post",  limit: 255
+    t.string   "title",      limit: 255
+    t.float    "price",      limit: 24
+    t.float    "area",       limit: 24
+    t.string   "address",    limit: 255
+    t.string   "picture",    limit: 255
+  end
+
+  add_index "posts", ["user_id", "created_at"], name: "index_posts_on_user_id_and_created_at", using: :btree
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id", using: :btree
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer  "liker_id",   limit: 4
+    t.integer  "liked_id",   limit: 4
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name",            limit: 255
@@ -24,4 +48,5 @@ ActiveRecord::Schema.define(version: 20150930040145) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
+  add_foreign_key "posts", "users"
 end
